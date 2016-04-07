4962cc3f5395fbe81c1aab9d3f7f2cd0f4560649
Damian Hickey <dhickey@gmail.com>
2016-04-07 13:38:13 +0200

    Update psake, fix build.

--- build.ps1
+++ build.ps1
@@ -11,12 +11,12 @@ if(Test-Path Env:\APPVEYOR_BUILD_NUMBER){
 
 $packageConfigs = Get-ChildItem . -Recurse | where{$_.Name -eq "packages.config"}
 foreach($packageConfig in $packageConfigs){
-	Write-Host "Restoring" $packageConfig.FullName 
-	src\.nuget\nuget.exe i $packageConfig.FullName -o src\packages
+	Write-Host "Restoring" $packageConfig.FullName
+	src\.nuget\nuget.exe install $packageConfig.FullName -o src\packages
 }
 
-Import-Module .\src\packages\psake.4.4.1\tools\psake.psm1
+Import-Module .\src\packages\psake.4.6.0\tools\psake.psm1
 Import-Module .\BuildFunctions.psm1
-Invoke-Psake .\default.ps1 default -framework "4.0x64" -properties @{ buildNumber=$buildNumber }
+Invoke-Psake .\default.ps1 default -framework "4.5.1x64" -properties @{ buildNumber=$buildNumber }
 Remove-Module BuildFunctions
 Remove-Module psake
--- default.ps1
+++ default.ps1
@@ -1,63 +1,63 @@
 properties {
-	$projectName = "LimitsMiddleware"
-	$buildNumber = 0
-	$rootDir  = Resolve-Path .\
-	$buildOutputDir = "$rootDir\build"
-	$reportsDir = "$buildOutputDir\reports"
-	$srcDir = "$rootDir\src"
-	$solutionFilePath = "$srcDir\$projectName.sln"
-	$assemblyInfoFilePath = "$srcDir\SharedAssemblyInfo.cs"
-	$ilmerge_path = "$srcDir\packages\ilmerge.2.14.1208\tools\ilmerge.exe"
+    $projectName = "LimitsMiddleware"
+    $buildNumber = 0
+    $rootDir  = Resolve-Path .\
+    $buildOutputDir = "$rootDir\build"
+    $reportsDir = "$buildOutputDir\reports"
+    $srcDir = "$rootDir\src"
+    $solutionFilePath = "$srcDir\$projectName.sln"
+    $assemblyInfoFilePath = "$srcDir\SharedAssemblyInfo.cs"
+    $ilmerge_path = "$srcDir\packages\ilmerge.2.14.1208\tools\ilmerge.exe"
 }
 
 task default -depends Clean, UpdateVersion, ILMerge, CreateNuGetPackages, RunTests
 
 task Clean {
-	Remove-Item $buildOutputDir -Force -Recurse -ErrorAction SilentlyContinue
-	exec { msbuild /nologo /verbosity:quiet $solutionFilePath /t:Clean }
+    Remove-Item $buildOutputDir -Force -Recurse -ErrorAction SilentlyContinue
+    exec { msbuild /nologo /verbosity:quiet $solutionFilePath /t:Clean }
 }
 
 task UpdateVersion {
-	$version = Get-Version $assemblyInfoFilePath
-	$oldVersion = New-Object Version $version
-	$newVersion = New-Object Version ($oldVersion.Major, $oldVersion.Minor, $oldVersion.Build, $buildNumber)
-	Update-Version $newVersion $assemblyInfoFilePath
+    $version = Get-Version $assemblyInfoFilePath
+    $oldVersion = New-Object Version $version
+    $newVersion = New-Object Version ($oldVersion.Major, $oldVersion.Minor, $oldVersion.Build, $buildNumber)
+    Update-Version $newVersion $assemblyInfoFilePath
 }
 
 task Compile {
-	exec { msbuild /nologo /verbosity:quiet $solutionFilePath /p:Configuration=Release }
+    exec { msbuild /nologo /verbosity:quiet $solutionFilePath /p:Configuration=Release }
 }
 
 task ILMerge -depends Compile {
-	$dllDir = "$srcDir\LimitsMiddleware\bin\Release"
-	$input_dlls = "$dllDir\LimitsMiddleware.dll"
-	Get-ChildItem -Path $dllDir -Filter *.dll |
-		foreach-object {
-			if ("$_" -ne "LimitsMiddleware.dll") {
-				$input_dlls = "$input_dlls $dllDir\$_"
-			}
-	}
+    $dllDir = "$srcDir\LimitsMiddleware\bin\Release"
+    $input_dlls = "$dllDir\LimitsMiddleware.dll"
+    Get-ChildItem -Path $dllDir -Filter *.dll |
+        foreach-object {
+            if ("$_" -ne "LimitsMiddleware.dll") {
+                $input_dlls = "$input_dlls $dllDir\$_"
+            }
+    }
 
-	New-Item $buildOutputDir -Type Directory -ErrorAction SilentlyContinue
-	Invoke-Expression "$ilmerge_path /targetplatform:v4 /internalize /allowDup /target:library /out:$buildOutputDir\$projectName.dll $input_dlls"
+    New-Item $buildOutputDir -Type Directory -ErrorAction SilentlyContinue
+    Invoke-Expression "$ilmerge_path /targetplatform:v4 /internalize /allowDup /target:library /out:$buildOutputDir\$projectName.dll $input_dlls"
 }
 
 task RunTests -depends Compile {
-	$xunitRunner = "$srcDir\packages\xunit.runner.console.2.0.0\tools\xunit.console.exe"
-	gci . -Recurse -Include *Tests.csproj, Tests.*.csproj | % {
-		$project = $_.BaseName
-		if(!(Test-Path $reportsDir\xUnit\$project)){
-			New-Item $reportsDir\xUnit\$project -Type Directory
-		}
+    $xunitRunner = "$srcDir\packages\xunit.runner.console.2.0.0\tools\xunit.console.exe"
+    gci . -Recurse -Include *Tests.csproj, Tests.*.csproj | % {
+        $project = $_.BaseName
+        if(!(Test-Path $reportsDir\xUnit\$project)){
+            New-Item $reportsDir\xUnit\$project -Type Directory
+        }
         exec { .$xunitRunner "$srcDir\$project\bin\Release\$project.dll" -html "$reportsDir\xUnit\$project\index.html" }
     }
 }
 
 task CreateNuGetPackages -depends Compile {
-	$versionString = Get-Version $assemblyInfoFilePath
-	$version = New-Object Version $versionString
-	$packageVersion = $version.Major.ToString() + "." + $version.Minor.ToString() + "." + $version.Build.ToString() + "-build" + $buildNumber.ToString().PadLeft(5,'0')
-	gci $srcDir -Recurse -Include *.nuspec | % {
-		exec { .$srcDir\.nuget\nuget.exe pack $_ -o $buildOutputDir -version $packageVersion }
-	}
+    $versionString = Get-Version $assemblyInfoFilePath
+    $version = New-Object Version $versionString
+    $packageVersion = $version.Major.ToString() + "." + $version.Minor.ToString() + "." + $version.Build.ToString() + "-build" + $buildNumber.ToString().PadLeft(5,'0')
+    gci $srcDir -Recurse -Include *.nuspec | % {
+        exec { .$srcDir\.nuget\nuget.exe pack $_ -o $buildOutputDir -version $packageVersion }
+    }
 }
