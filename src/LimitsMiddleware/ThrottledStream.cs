28e3f0682adda45e6f5e4eecf20d45ab67864b05
Damian Hickey <dhickey@gmail.com>
2016-05-02 21:44:36 +0200

    C#6-ify

--- src/LimitsMiddleware/ThrottledStream.cs
+++ src/LimitsMiddleware/ThrottledStream.cs
@@ -21,25 +21,13 @@
             _tokenBucket = fixedTokenBucket;
         }
 
-        public override bool CanRead
-        {
-            get { return _innerStream.CanRead; }
-        }
+        public override bool CanRead => _innerStream.CanRead;
 
-        public override bool CanSeek
-        {
-            get { return _innerStream.CanSeek; }
-        }
+        public override bool CanSeek => _innerStream.CanSeek;
 
-        public override bool CanWrite
-        {
-            get { return _innerStream.CanWrite; }
-        }
+        public override bool CanWrite => _innerStream.CanWrite;
 
-        public override long Length
-        {
-            get { return _innerStream.Length; }
-        }
+        public override long Length => _innerStream.Length;
 
         public override long Position
         {
