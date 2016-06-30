06ed773e92edd4979f7037199305eb4ee47415f3
Damian Hickey <dhickey@gmail.com>
2016-06-30 23:53:54 +0200

    Update AppBuilderExtensions.MinResponseDelay.cs

--- src/LimitsMiddleware.OwinAppBuilder/AppBuilderExtensions.MinResponseDelay.cs
+++ src/LimitsMiddleware.OwinAppBuilder/AppBuilderExtensions.MinResponseDelay.cs
@@ -6,12 +6,11 @@
     public static partial class AppBuilderExtensions
     {
         /// <summary>
-        ///     Sets a minimum delay before sending the response.
+        ///     Sets a minimum delay in miliseconds before sending the response.
         /// </summary>
         /// <param name="app">The IAppBuilder instance.</param>
         /// <param name="minDelay">
-        ///     The maximum number of bytes per second to be transferred. Use 0 or a negative
-        ///     number to specify infinite bandwidth.
+        ///     The minimum delay to wait before sending the response.
         /// </param>
         /// <param name="loggerName">(Optional) The name of the logger log messages are written to.</param>
         /// <returns>The IAppBuilder instance.</returns>
@@ -45,8 +44,8 @@
         /// </summary>
         /// <param name="app">The IAppBuilder instance.</param>
         /// <param name="getMinDelay">
-        ///     A delegate to retrieve the maximum number of bytes per second to be transferred.
-        ///     Allows you to supply different values at runtime. Use 0 or a negative number to specify infinite bandwidth.
+        ///     A delegate to retrieve the minimum delay before calling the next stage in the pipeline. Note:
+        ///     the delegate should return quickly.
         /// </param>
         /// <param name="loggerName">(Optional) The name of the logger log messages are written to.</param>
         /// <returns>The app instance.</returns>
@@ -64,8 +63,8 @@
         /// </summary>
         /// <param name="app">The IAppBuilder instance.</param>
         /// <param name="getMinDelay">
-        ///     A delegate to retrieve the maximum number of bytes per second to be transferred.
-        ///     Allows you to supply different values at runtime. Use 0 or a negative number to specify infinite bandwidth.
+        ///     A delegate to retrieve the minimum delay before calling the next stage in the pipeline. Note:
+        ///     the delegate should return quickly.
         /// </param>
         /// <returns>The app instance.</returns>
         public static IAppBuilder MinResponseDelay(this IAppBuilder app, Func<RequestContext, TimeSpan> getMinDelay,
